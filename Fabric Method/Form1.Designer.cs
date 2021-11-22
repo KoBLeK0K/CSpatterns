e93762bb1f114790c4dbd1b529df0123c8782a45
KoBLeK0K <75327213+KoBLeK0K@users.noreply.github.com>
2021-11-22 23:06:45 +0300

    Add files via upload

---
+++ Fabric Method/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace Fabric_Method
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numberOfSights = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 119);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 319);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(249, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Angles amount";
+            // 
+            // numberOfSights
+            // 
+            this.numberOfSights.Location = new System.Drawing.Point(393, 42);
+            this.numberOfSights.MaxLength = 1;
+            this.numberOfSights.Name = "numberOfSights";
+            this.numberOfSights.Size = new System.Drawing.Size(100, 23);
+            this.numberOfSights.TabIndex = 3;
+            this.numberOfSights.TextChanged += new System.EventHandler(this.numberOfSights_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numberOfSights);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numberOfSights;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Fabric Method/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fabric_Method
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int getHeight()
+        {
+            return pictureBox1.Height;
+        }
+        public int getWidth()
+        {
+            return pictureBox1.Width;
+        }
+
+        private void numberOfSights_TextChanged(object sender, EventArgs e)
+        {
+        
+            if (numberOfSights.Text is null)
+                numberOfSights.Text = "0";
+            ShapeFactory factory = new ShapeFactory();
+            Shape shapePolygon = ShapeFactory.GetShape(Convert.ToInt32(numberOfSights.Text));
+            if(Convert.ToInt32(numberOfSights.Text) <= 5)
+            pictureBox1.Image = shapePolygon.Draw();
+            else
+            {
+                MessageBox.Show("Error. Enter num from 0 to 5");
+                numberOfSights.Text = "0";
+            }
+            label2.Text = shapePolygon.Descriptor();
+        }
+    }
+    public class ShapeFactory
+    {
+        public static Shape GetShape(int numbOfSides)
+        {
+            switch (numbOfSides)
+            {
+                case 0:
+                    return new Ellipse();
+                case 1:
+                    return new Line();
+                case 2:
+                    return new Angle();
+                case 3:
+                    return new Triangle();
+                case 4:
+                    return new Rectangle();
+
+                default: return new Pentagon();
+            }
+
+        }
+    }
+    abstract public class Shape
+    {
+        public int x1;
+        public int y1;
+        public SolidBrush pen;
+        public Pen myPen;
+
+        public Shape()
+        {
+            x1 = 40;
+            y1 = 40;
+            pen = new SolidBrush(Color.White);
+            myPen = new Pen(Color.Blue);
+        }
+        abstract public Image Draw();
+        abstract public string Descriptor();
+    }
+
+    class Ellipse : Shape
+    {
+       
+        public override Image Draw()
+        {
+            Form1 form1 = new Form1();
+            Bitmap pictureBox = new Bitmap(form1.getWidth(), form1.getHeight());
+            Graphics graph = Graphics.FromImage(pictureBox);
+            graph.FillRectangle(pen, form1.ClientRectangle);
+            graph.DrawEllipse(myPen, x1, y1,100 ,100 );
+            return pictureBox;
+        }
+        public override string Descriptor()
+        {
+            return "Circle";
+        }
+    }
+    class Line : Shape
+    {
+
+        public override Image Draw()
+        {
+            Form1 form1 = new Form1();
+            Bitmap pictureBox = new Bitmap(form1.getWidth(), form1.getHeight());
+            Graphics graph = Graphics.FromImage(pictureBox);
+            graph.FillRectangle(pen, form1.ClientRectangle);
+            graph.DrawLine(myPen, x1, y1, 200, 200);
+            return pictureBox;
+        }
+        public override string Descriptor()
+        {
+            return "Line";
+        }
+    }
+    class Angle : Shape
+    {
+        public override Image Draw()
+        {
+            Form1 form1 = new Form1();
+            Bitmap pictureBox = new Bitmap(form1.getWidth(), form1.getHeight());
+            Graphics graph = Graphics.FromImage(pictureBox);
+            graph.FillRectangle(pen, form1.ClientRectangle);
+            graph.DrawLine(myPen, 100, 100, 200, 200);
+            graph.DrawLine(myPen, 200, 200, 100, 200);
+            return pictureBox;
+        }
+        public override string Descriptor()
+        {
+            return "Angle";
+        }
+    }
+      
+    class Triangle : Shape
+    {
+        
+        public override Image Draw()
+        {
+            Form1 form1 = new Form1();
+            Bitmap pictureBox = new Bitmap(form1.getWidth(), form1.getHeight());
+            Graphics graph = Graphics.FromImage(pictureBox);
+            graph.FillRectangle(pen, form1.ClientRectangle);
+
+            PointF p1 = new PointF(100, 100);
+            PointF p2 = new PointF(200, 200);
+            PointF p3 = new PointF(200, 100);
+            PointF[] curvePoints =
+            {
+                p1,p2,p3
+            };
+            graph.DrawPolygon(myPen, curvePoints);
+            return pictureBox;
+        }
+        public override string Descriptor()
+        {
+            return "Triangle";
+        }
+    }
+
+    class Rectangle : Shape
+    {
+
+        public override Image Draw()
+        {
+            Form1 form1 = new Form1();
+            Bitmap pictureBox = new Bitmap(form1.getWidth(), form1.getHeight());
+            Graphics graph = Graphics.FromImage(pictureBox);
+            graph.FillRectangle(pen, form1.ClientRectangle);
+            graph.DrawRectangle(myPen, x1, y1, 200, 200);
+            return pictureBox;
+        }
+        public override string Descriptor()
+        {
+            return "Rectangle";
+        }
+    }
+
+    class Pentagon : Shape
+    {
+        public override Image Draw()
+        {
+            Form1 form1 = new Form1();
+            Bitmap pictureBox = new Bitmap(form1.getWidth(), form1.getHeight());
+            Graphics graph = Graphics.FromImage(pictureBox);
+            graph.FillRectangle(pen, form1.ClientRectangle);
+            PointF p1 = new PointF(150,100);
+            PointF p2 = new PointF(200,150);
+            PointF p3 = new PointF(175,200);
+            PointF p4 = new PointF(125,200);
+            PointF p5 = new PointF(100,150);
+
+            PointF[] curvePoints =
+            {
+                p1,p2,p3,p4,p5
+            };
+            graph.DrawPolygon(myPen, curvePoints);
+            return pictureBox;
+        }
+        public override string Descriptor()
+        {
+            return "Pentagon";
+        }
+    }
+}
